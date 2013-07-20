be2bfe8e1cc8cfdef29d984c5dfb0efbeee7262d
Nikita Gusev <gusevnikita@hotmail.com>
2013-07-20 16:39:41 +0300

    Second commit

---
+++ ServiceTemplate/ServiceTemplate.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace ServiceTemplate
+{
+    public abstract class ServiceTemplate<TService, TInterface>
+        where TService : System.ServiceModel.ClientBase<TInterface>
+        where TInterface : class
+    {
+        private TService Service
+        {
+            get
+            {
+                return Activator.CreateInstance<TService>();
+            }
+        }
+
+        public TResult MethodTemplate<TResult>(Func<TService, TResult> method)
+        {
+            var service = Service;
+            try
+            {
+                var result = method(service);
+                service.Close();
+                return result;
+            }
+            finally
+            {
+                if (service.State != CommunicationState.Closed)
+                    service.Abort();
+            }
+        }
+
+        public void MethodTemplate(Action<TService> method)
+        {
+            var service = Service;
+            try
+            {
+                method(service);
+                service.Close();
+            }
+            finally
+            {
+                if (service.State != CommunicationState.Closed)
+                    service.Abort();
+            }
+        }
+    }
+}
